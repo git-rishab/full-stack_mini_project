fca8cd567cff3081914a41b418801b7c7231140c
rishab <rkc3660@gmail.com>
2023-02-18 01:23:49 +0530

    update

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.get("/",(req,res)=>{
+    res.send("Welcome to homepage");
+})
 app.use("/user",userRoute);
 
 app.use(authenticate);
